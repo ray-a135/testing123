7f0a5f2f3beaca630420c21bfe4968e3d51dfad5
ray-a135 <rachelauguste36@gmail.com>
2025-04-15 07:25:02 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "swords":return img`
+. . . . . . . . . . . . . . . . 
+. . b . . . . . . b b . . . . . 
+. . b b . . . . . b b . . . . . 
+. . b b b . . . . b b b . . . . 
+. . b b b . . . . b b b . . . . 
+. . b b b . . . . b b b . . . . 
+. . b b b . . . . b b b . . . . 
+. . b b b . . . . b b . . . . . 
+. . b b . . . . . b b . . . . . 
+. . b b . . . . . b b . . . . . 
+. . b . . . . . . b . . . . . . 
+. e e e e . . . e e e e . . . . 
+. . e e . . . . . e e . . . . . 
+. . e . . . . . . e . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
