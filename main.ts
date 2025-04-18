800413e3c49f4c641cbb8c897e2c79588531655e
ray-a135 <rachelauguste36@gmail.com>
2025-04-18 13:56:11 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,25 @@
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+mp.onButtonEvent(mp.MultiplayerButton.B, ControllerButtonEvent.Pressed, function (player2) {
     projectile = sprites.createProjectileFromSprite(assets.image`fireball`, mySprite, 50, 50)
 })
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(assets.image`swords`, mySprite2, 50, 50)
-})
-function win () {
-	
+function movement () {
+    mySprite3.setPosition(randint(0, 100), randint(20, 60))
+    mySprite3.setVelocity(50, 50)
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function (player2) {
+    projectile = sprites.createProjectileFromSprite(assets.image`swords`, mySprite8, 50, 50)
+})
 info.onLifeZero(function () {
     game.gameOver(false)
     game.setGameOverEffect(false, effects.clouds)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
-	
-})
 let projectile: Sprite = null
-let mySprite2: Sprite = null
+let mySprite3: Sprite = null
+let mySprite8: Sprite = null
 let mySprite: Sprite = null
+splitScreen.setSplitScreenEnabled(true)
 music.play(music.createSong(hex`0078000408020900001c00010a006400f401640000040000000000000000000000000005000004240010001400012c18001c0001271c002000012a20002400012c28002c0001252c003000012901001c000f05001202c102c201000405002800000064002800031400060200041f000000040001190c001000011b14001800011e18001c00011d1c002000021b2002001c000c960064006d019001000478002c010000640032000000000a060005270004000800012008000c00011e24002800012c28002c00012730003400041b24272c34003800012403001c0001dc00690000045e01000400000000000000000000056400010400030f00200024000224272c0030000320252c04001c00100500640000041e000004000000000000000000000000000a040004290008000c0002192c10001400021b2014001800012918001c000319222c2400280002252a28002c00012005001c000f0a006400f4010a0000040000000000000000000000000000000002060014001800012406001c00010a006400f40164000004000000000000000000000000000000000225000000040002242c04000800012708000c0001200c00100001271c002000012520002400011d08001c000e050046006603320000040a002d0000006400140001320002010002060008000c00012509010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c80024000400050001090c000d00010b1000110001092800290001012c002d000100340035000104`), music.PlaybackMode.LoopingInBackground)
 scene.setBackgroundImage(img`
     3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
@@ -160,11 +163,10 @@ mySprite = sprites.create(img`
     2 . . . . . . . . . . . . . . 2 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     `, SpriteKind.Player)
-scene.cameraFollowSprite(mySprite)
 controller.player1.moveSprite(mySprite)
-let mySprite8 = sprites.create(assets.image`player 2`, SpriteKind.Player)
+mySprite8 = sprites.create(assets.image`player 2`, SpriteKind.Player)
 controller.player2.moveSprite(mySprite8)
-let mySprite3 = sprites.create(img`
+mySprite3 = sprites.create(img`
     2 2 2 2 f f f f f f f 3 2 2 2 2 
     2 2 . f f f f f f f f f f . 2 2 
     2 2 f . f f f f f f f f . f 2 2 
@@ -182,6 +184,10 @@ let mySprite3 = sprites.create(img`
     2 . . . . f . . . . . f . . . 2 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     `, SpriteKind.Enemy)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, mySprite)
+splitScreen.setCameraRegion(splitScreen.Camera.Camera1, splitScreen.CameraRegion.HorizontalTopHalf)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, mySprite8)
+splitScreen.setCameraRegion(splitScreen.Camera.Camera2, splitScreen.CameraRegion.HorizontalBottomHalf)
 mySprite3.setPosition(78, 44)
 let mySprite4 = sprites.create(img`
     . f f 8 8 8 8 f f f 8 8 8 8 8 . 
@@ -201,8 +207,8 @@ let mySprite4 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Enemy)
-mySprite4.setPosition(72, 50)
-mySprite2 = sprites.create(img`
+mySprite4.setPosition(29, 28)
+let mySprite2 = sprites.create(img`
     7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
     7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
     7 7 7 7 2 f 2 7 7 7 2 f 2 7 7 7 
@@ -224,11 +230,6 @@ mySprite2.setPosition(60, 40)
 let mySprite5 = sprites.create(assets.image`lava`, SpriteKind.Projectile)
 let mySprite6 = sprites.create(assets.image`swords`, SpriteKind.Projectile)
 let mySprite7 = sprites.create(assets.image`fireball`, SpriteKind.Projectile)
-game.onUpdateInterval(1000, function () {
-    mySprite3.setPosition(randint(20, 60), randint(20, 60))
-    mySprite3.setVelocity(50, 50)
-    mySprite2.setPosition(randint(20, 60), randint(20, 60))
-    mySprite2.setVelocity(50, 50)
-    mySprite4.setPosition(randint(20, 60), randint(20, 60))
-    mySprite4.setVelocity(50, 50)
-})
+mySprite2.follow(mySprite, 40)
+mySprite3.follow(mySprite, 40)
+mySprite4.follow(mySprite8, 40)
